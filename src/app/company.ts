bffbab1708f9149bd8b27e2e14f8a9f1ffbd826c
Miao Jianhui <miaojianhui@inspur.com>
2018-08-14 15:58:59 +0800

    my first git test

--- src/app/company.ts
+++ src/app/company.ts
@@ -1,6 +1,9 @@
-export class Company{
-    id:number;
-    name:string;
-    established_time:string;
-    main_business:string;
+/**
+ * 实体类
+ */
+export class Company {
+    id: number;
+    name: string;
+    established_time: string;
+    main_business: string;
 }
